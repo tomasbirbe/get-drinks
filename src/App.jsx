4f1d9f6633db3d48647d1304ec68ce604bdff21a
Tomas Birbe <tomas.birbe@gmail.com>
2021-08-19 21:21:20 -0300

    Split code, add react router and make drink's page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Axios from "axios";
-import { useEffect, useState } from "react";
-import RandomDrink from "./components/random-drink"
-import "./styles/App.css";
+import Home from "./pages/home/index";
+import Drink from "./pages/drink/index"
+import { useState } from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const DRINK_INITIALIZE = {
   id:0,
@@ -9,89 +9,21 @@ const DRINK_INITIALIZE = {
   img:""
 }
 
-let isFirstRender = true
 
-function App() {
-  const [drinkName, setDrinkName] = useState("")
-  const [randomDrink, setRandomDrink] = useState(DRINK_INITIALIZE)
-  const [drinks, setDrinks] = useState([])
-  const [loading, setLoading] = useState(false)
-  
-  useEffect(() => {
-    if(isFirstRender === true){
-      isFirstRender = false
-      getRandomDrink()
-    } 
+export default function App() {
+  const [drink, setDrink] = useState({
+
   })
 
-  function getRandomDrink(){
-    setLoading(true) 
-    Axios({
-      method:"get",
-      url:`https://www.thecocktaildb.com/api/json/v1/1/random.php`
-    })
-    .then(({data}) => {
-      let newDrink = {
-        id:data.drinks[0].idDrink,
-        name:data.drinks[0].strDrink,
-        img:data.drinks[0].strDrinkThumb
-      }
-      setLoading(false)
-      setRandomDrink(newDrink)
-    })
-  }
+  return(
+    <Router>
+      <Route path="/" exact>
+        <Home setterDrink={setDrink}/>
+      </Route>
+      <Route path="/:idDrink" exact>
+        <Drink drink={drink}></Drink>
+      </Route>
+    </Router>
 
-  function getDrink(event){
-    if(event.key === "Enter" && event.target.value !== ''){
-      Axios({
-        method:"get",
-        url:`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`
-      })
-      .then( ({data}) => {
-        setDrinks([])
-        if(data.drinks !== null){
-          console.log(data.drinks)
-          setDrinks([...data.drinks])
-        } else {
-          setDrinks([])
-        }
-      })
-    } else if(event.target.value === ''){  
-      setDrinks([])
-    } else {
-      setDrinkName(event.target.value.trim())
-    }
-  }
-
-  return (
-    <div className="App">
-      <div className="searchbar">
-        <div className="searchbar--container-searchbar">
-          <span className="searchbar--container-searchbar--search-icon"></span>
-          <input type="text" onKeyPress={getDrink} className="searchbar--container-searchbar--input"/>
-          <span className="searchbar--container-searchbar--filter-icon"></span>
-        </div>
-        {
-          drinks.length !== 0 ?
-          <div className="searchbar--container-results">
-            <ul className="searchbar--container-results--results">
-              {
-                drinks.map((element,index) => <li key={index} className="searchbar--container-results--results--item">{element.strDrink}</li>)
-              }
-            </ul>
-          </div>
-          : ''
-        }
-      </div>
-
-      {randomDrink.length !== 0
-      ? <RandomDrink drink={randomDrink} isLoading={loading}></RandomDrink>
-      : ''
-      }
-
-      <button className="random-btn" onClick={getRandomDrink}>Random drinks</button>
-    </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,115 @@
+import Axios from "axios";  
+import { useEffect, useState } from "react";
+import RandomDrink from "../../components/random-drink"
+import "../../styles/App.css";
+import { BrowserRouter as Router, Link, history } from "react-router-dom";
+
+const DRINK_INITIALIZE = {
+  id:0,
+  name:"",
+  img:""
+}
+
+let isFirstRender = true
+
+export default function Home({setterDrink}) {
+  const [drinkName, setDrinkName] = useState("")
+  const [randomDrink, setRandomDrink] = useState(DRINK_INITIALIZE)
+  const [drinks, setDrinks] = useState([])
+  const [loading, setLoading] = useState(false)
+  
+  useEffect(() => {
+    if(isFirstRender === true){
+      isFirstRender = false
+      getRandomDrink()
+    } 
+  })
+
+  function getRandomDrink(){
+    setLoading(true) 
+    Axios({
+      method:"get",
+      url:`https://www.thecocktaildb.com/api/json/v1/1/random.php`
+    })
+    .then(({data}) => {
+      let newDrink = {
+        id:data.drinks[0].idDrink,
+        name:data.drinks[0].strDrink,
+        img:data.drinks[0].strDrinkThumb
+      }
+      setLoading(false)
+      setRandomDrink(newDrink)
+    })
+  }
+
+  const handleChange = event => {
+    event.preventDefault();
+    setDrinkName(event.target.value);
+  }
+
+  const getDrinks = (event) => {
+    if(event.target.value !== ''){
+      Axios({
+        method:"get",
+        url:`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`
+      })
+      .then( ({data}) => {
+        setDrinks([])
+        if(data.drinks !== null){
+          console.log(data.drinks)
+          setDrinks([...data.drinks])
+        } else {
+          setDrinks([])
+        }
+      })
+    } else {  
+      setDrinks([])
+    }
+    event.preventDefault();
+  }
+
+  const showDrink = (history) => {
+    return function(){
+      console.log("hola")
+      setterDrink(drink)
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className="searchbar">
+        <form className="searchbar--container-searchbar" onSubmit={getDrinks}>
+          <span className="searchbar--container-searchbar--search-icon"></span>
+          <input type="text" id="searchbar" onChange={handleChange} className="searchbar--container-searchbar--input" autoComplete="off"/>
+          <span className="searchbar--container-searchbar--filter-icon"></span>
+        </form>
+        {
+          drinks.length !== 0 ?
+          <div className="searchbar--container-results">
+            <ul className="searchbar--container-results--results">
+              {
+                drinks.map((element,index) => {
+                  return (
+                    <button key={index} onClick={showDrink(element)}>
+                      <li className="searchbar--container-results--results--item">
+                        <Link to={`/${element.idDrink}`}>{element.strDrink}</Link>
+                      </li>
+                    </button>
+                  )
+                })
+              }
+            </ul>
+          </div>
+          : ''
+        }
+      </div>
+
+      {randomDrink.length !== 0
+      ? <RandomDrink drink={randomDrink} isLoading={loading}></RandomDrink>
+      : ''
+      }
+
+      <button className="random-btn" onClick={getRandomDrink}>Random drinks</button>
+    </div>
+  );
+}
